31c1817b161499ccc83df0e49416fbc4c18b0275
stivenVA <stivenv.ariza@gmail.com>
2024-09-19 21:25:14 -0500

    si

---
+++ src/main/java/org/example/Views/Desktop.java
@@ -0,0 +1,11 @@
+package org.example.Views;
+
+import javax.swing.*;
+
+public class Desktop {
+    private JTabbedPane tabbedPane1;
+    private JPanel panel1;
+    private JTabbedPane tabbedPane3;
+    private JTextField textField1;
+    private JTextField textField2;
+}
